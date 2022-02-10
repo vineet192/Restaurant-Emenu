2295d56c7fb388cee94733ad1c9df764d0d212dc
Vineet Kalghatgi <vkalghat@gmail.com>
2022-02-10 23:49:06 +0530

    refactor menu to menu-config

--- pages/menu-config/[menuID].js
+++ pages/menu-config/[menuID].js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
 import { useRouter } from 'next/router';
+import { useAuth } from '../../contexts/AuthContext';
 import NavBar from '../../components/nav';
 import MenuForm from '../../components/menu';
 
 export default function Home() {
   const { currentUser } = useAuth();
   const router = useRouter();
+  const { menuID } = router.query;
   useEffect(() => {
+    console.log(menuID);
     if (!currentUser) {
       router.push('/login');
     }
