94e7bf430f733b02df3884c23e368f1b1ddcf007
Vineet Kalghatgi <vkalghat@gmail.com>
2022-04-24 01:38:12 +0530

    display dishes and implement dishes popup in menu

--- pages/menu/[menuID].js
+++ pages/menu/[menuID].js
@@ -13,9 +13,7 @@ export default function () {
   const categoriesPopup = useRef();
 
   useEffect(async () => {
-    let res = await fetch(
-      `${SERVER_URL}/menu?menuID=${menuID}`
-    );
+    let res = await fetch(`${SERVER_URL}/menu?menuID=${menuID}`);
     let data = await res.json();
     console.log(data.menu);
     setMenuCard((prevState) => data.menu);
@@ -27,20 +25,37 @@ export default function () {
 
   return (
     <div>
-      <button
-        className="absolute bottom-0 right-0 m-5 p-2 flex bg-blue-500 text-white rounded w-fit"
-        onClick={handleOpenCategories}>
-        <h1>Menu</h1>
-      </button>
+      <div className="w-4/5 m-5 p-2 flex flex-col">
+        {menuCard.categories.map((category, categoryIndex) => (
+          <div
+            className="flex flex-col my-10 mx-2"
+            key={categoryIndex}
+            onClick={toggleDishes}>
+            <div className="rounded-md p-2 shadow-md cursor pointer">
+              <h1 className="text-4xl text-blue-600 font-extrabold">
+                {category.title}
+              </h1>
+            </div>
+            <hr />
+            <div className="hidden p-2" id="dish-list">
+              {category.dishes.map((dish, dishIndex) => (
+                <div className="flex flex-col my-2" key={dishIndex}>
+                  <h1 className="text-2xl">{dish.dishName}</h1>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
 
       <div
-        className="hidden flex-col items-center bg-blue-500 rounded-lg text-white fixed bottom-0 right-0 p-2 m-5"
+        className="hidden z-10 flex-col items-center bg-blue-500 rounded-lg text-white fixed bottom-0 right-0 p-2 m-5"
         ref={categoriesPopup}>
-        <h1>Categories</h1>
+        <h1 className="text-2xl font-extrabold">Categories</h1>
         <ul className="my-5">
           {menuCard.categories.map((category, index) => (
-            <li key={index}>
-              <h1>{category.title}</h1>
+            <li key={index} className="my-2">
+              <h1 className="text-2xl">{category.title}</h1>
             </li>
           ))}
         </ul>
@@ -50,6 +65,12 @@ export default function () {
           <FontAwesomeIcon icon={faBackspace}></FontAwesomeIcon>
         </button>
       </div>
+
+      <button
+        className="fixed z-5 bottom-0 right-0 m-5 p-2 bg-blue-500 text-white rounded w-fit"
+        onClick={handleOpenCategories}>
+        <h1>Menu</h1>
+      </button>
     </div>
   );
 
@@ -59,5 +80,10 @@ export default function () {
 
   function handleOpenCategories(event) {
     categoriesPopup.current.classList.replace('hidden', 'flex');
+  }
+
+  function toggleDishes(event) {
+    let dishList = event.currentTarget.querySelector('#dish-list');
+    dishList.classList.toggle('hidden');
   }
 }
